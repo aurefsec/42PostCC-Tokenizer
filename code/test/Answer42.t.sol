3c763a80f880e021fd14d44ce36e65e795209199
aurefsec <auref.contact@protonmail.com>
2026-09-17 00:19:10 +0200

    testTransfer()

--- code/test/Answer42.t.sol
+++ code/test/Answer42.t.sol
@@ -8,11 +8,18 @@ contract Answer42Test is Test
 {
   // All the functions declares here can be used everywhere in the contract
   Answer42 asr;
-  uint256 initialSupply = 1000;
+  uint256 initialSupply;
+  address myAddr;
+  address userAddr1;
+  address userAddr2;
 
   function setUp() public
   {
+    initialSupply = 1000;
     asr = new Answer42(initialSupply);
+    myAddr = address(this);
+    userAddr1 = makeAddr("userAddr1");
+    userAddr2 = makeAddr("userAddr2");
   }
 
   // All the test functions have to start by "test" keyword
@@ -22,5 +29,16 @@ contract Answer42Test is Test
     assertEq(asr.name(), "Answer42");
     assertEq(asr.symbol(), "ASR");
     assertEq(asr.totalSupply(), initialSupply);
+    assertEq(asr.balanceOf(address(this)), initialSupply);
+  }
+
+  function testTransfer() public
+  {
+    asr.transfer(userAddr1, 100);
+    asr.transfer(userAddr2, 100);
+
+    assertEq(asr.balanceOf(address(this)), 800);
+    assertEq(asr.balanceOf(address(userAddr1)), 100);
+    assertEq(asr.balanceOf(address(userAddr2)), 100);
   }
 }
